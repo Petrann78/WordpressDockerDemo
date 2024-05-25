42415850e7d1912b87ddf876b11e7be787403b3c
pbouf <p.boufidis1@gmail.com>
2024-05-25 23:25:27 +0300

    remove unnecessary docker build

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 echo -e "Initializing Wordpress Service...\n"
 echo -e "Building the docker image please wait...\n"
-docker build -t wordpress-updated:1.0 .
 echo -e "\nRunning Docker compose pull\n"
 docker compose -f docker-compose.yml pull
 echo -e "\nRunning Docker compose up\n"
